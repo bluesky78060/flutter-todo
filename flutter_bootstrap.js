be4ba4b71715096d912a11acb60d7abfe0cbb9d3
bluesky78060 <bluesky78060@users.noreply.github.com>
2025-11-16 07:44:28 +0000

    deploy: 216a665dcd8e91b8d155d786f880c3b0505f36e4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1577008802"
+    serviceWorkerVersion: "3030232167"
   }
 });
