60605476e4227a39949a7fac35e02d2541f3a397
bluesky78060 <bluesky78060@users.noreply.github.com>
2025-11-16 06:56:09 +0000

    deploy: e80a04333711c3a36a91f083354f43ed6345459e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"sql-wasm.wasm": "8f5029a1d3545424d4f25647673e5e32",
 "sqlite3.wasm": "fa7637a49a0e434f2a98f9981856d118",
 "sql-wasm.js": "6bd05b179c947cce4eea15193868ef14",
 "404.html": "2aca34fca096ea4d005b866dba8508bf",
-"flutter_bootstrap.js": "132a2208029afd8376e270b1a8cbdf4e",
+"flutter_bootstrap.js": "ed9348dc101c16f9becc1fbd1f858701",
 "version.json": "2ec1eab9e252d89d699d1af0fe30590a",
 "main.dart.js": "e6d36e009966d7fda50eeebb71452ee2"};
 // The application shell files that are downloaded before a service worker can
