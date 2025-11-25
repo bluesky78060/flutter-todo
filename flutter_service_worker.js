2456be6a08d5a15a346788c106986fcc97a239a8
bluesky78060 <bluesky78060@users.noreply.github.com>
2025-11-25 12:22:19 +0000

    deploy: ccc8588aa87e09a717bdeeb0e06c58d1932411e1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,7 +48,7 @@ const RESOURCES = {"sql-wasm.wasm": "8f5029a1d3545424d4f25647673e5e32",
 "naver_map_diagnostic.html": "8fb381c9813f37c85303092f5610dcbb",
 "sql-wasm.js": "6bd05b179c947cce4eea15193868ef14",
 "404.html": "6f797132280938311cb25cfd924321da",
-"flutter_bootstrap.js": "f2ff279a33124f90c96b28369d319c9b",
+"flutter_bootstrap.js": "e4644d56d490bf0bfdc0465ab465d85d",
 "version.json": "407bc5aa18b54eee3101137af6aab81f",
 "naver_map_bridge.js": "7172b9ca94791a91e881f6e1e2427e40",
 "main.dart.js": "1a0297f4835cc52bcea340283ba561fd"};
