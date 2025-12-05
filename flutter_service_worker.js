43bbca19d44967ea3975cdd8b9a55830534b8b3a
bluesky78060 <bluesky78060@users.noreply.github.com>
2025-12-05 12:13:02 +0000

    deploy: 39527333c023ac4ad2b0bb4a4bf400d6d3c931bd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,10 +57,10 @@ const RESOURCES = {"sql-wasm.wasm": "8f5029a1d3545424d4f25647673e5e32",
 "naver_map_diagnostic.html": "8fb381c9813f37c85303092f5610dcbb",
 "sql-wasm.js": "6bd05b179c947cce4eea15193868ef14",
 "404.html": "6f797132280938311cb25cfd924321da",
-"flutter_bootstrap.js": "38fcf22f2bb96692a5bb1a9cc98d2926",
+"flutter_bootstrap.js": "49f368ca4f02ea92be04fcc79fb5f1c6",
 "version.json": "15d8657706d3de9f9c2621d89895f8a8",
 "naver_map_bridge.js": "7172b9ca94791a91e881f6e1e2427e40",
-"main.dart.js": "43964f64fab7482ba0e6d8bcaf3c7846"};
+"main.dart.js": "efd145ecd8233f7d800fb65273141a54"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
